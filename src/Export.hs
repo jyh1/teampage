6104098fe17a7cef65c74362f63c6f7b1f1d4713
jyh1 <jyh1@mail.ustc.edu.cn>
2017-03-03 10:30:00 +0800

    test hamlet file

--- src/Export.hs
+++ src/Export.hs
@@ -1,1 +1,9 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Export where
+
+import Text.Hamlet (shamletFile)
+import Text.Blaze.Html.Renderer.String (renderHtml)
+
+
+test :: String
+test = renderHtml $(shamletFile "template.hamlet")
